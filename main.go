fb682e0b0dbc40f6f4c8ec24ef0139257c2da0af
alekslesik <alekslesik@gmail.com>
2024-01-28 17:12:28 +0300

    net/http/httptest

--- main.go
+++ main.go
@@ -1,50 +1,43 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"io"
 	"log"
-	"time"
-
-	"github.com/alekslesik/config"
-	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"net/http/httptest"
 )
 
-
 func main() {
-	db, err := sql.Open("mysql", "root:486464@/kinovdom")
-	if err != nil {
-		panic(err)
-	}
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
 
-	// See "Important settings" section.
-	db.SetConnMaxLifetime(time.Minute * 3)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
+	ts.Start()
+	defer ts.Close()
 
-	rows, err := db.Query("SELECT ID FROM b_catalog_product")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	client := ts.Client()
 
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		err := rows.Scan(&id)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println(id)
-	}
-
-	err = rows.Close()
+	res1, err := client.Get("https://example.com")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	config.New()
+	r, err := io.ReadAll(res1.Body)
+	res1.Body.Close()
 
+	fmt.Println(string(r))
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	greeting, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", greeting)
 }
-

b726bce23705830d6ef58583d4c86b208e41241b
alekslesik <alekslesik@gmail.com>
2024-03-09 15:13:10 +0300

    net/http/httputil

--- main.go
+++ main.go
@@ -1,79 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
-
-	"githhub.com/alekslesik/learn/kandinsky"
+	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
 )
 
-// https://api-key.fusionbrain.ai/key/api/v1/text2image/run
-// FFDB0757E5E5A2FF2D7A297CE95BDA2D
-// F9C4980BC3166E19C9A42607358B8DA6
-
 func main() {
-	key := "FFDB0757E5E5A2FF2D7A297CE95BDA2D"
-	secret := "F9C4980BC3166E19C9A42607358B8DA6"
+	backendServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "this call was relayed by the reverse proxy")
+	}))
+	defer backendServer.Close()
 
-	p := kandinsky.Params{
-		Type:           "GENERATE",
-		Style:          "UHD",
-		Width:          1024,
-		Height:         1024,
-		NumImages:      1,
-		NegativePrompt: "",
-		GenerateParams: struct {
-			Query string "json:\"query\""
-		}{"Обнаружены доказательства гипотезы РНК-мира"},
+	rpURL, err := url.Parse(backendServer.URL)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	image := make(chan kandinsky.Image)
-
-	go func() {
-		for {
-			i, err := kandinsky.GetImage(key, secret, p)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			image <- i
-		}
-	}()
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
+	frontendProxy := httptest.NewServer(&httputil.ReverseProxy{
+		Rewrite: func(r *httputil.ProxyRequest) {
+			r.SetXForwarded()
+			r.SetURL(rpURL)
+		},
 	})
+	defer frontendProxy.Close()
 
-	errCh := make(chan error)
-
-	go func() {
-		err := http.ListenAndServe("localhost:6666", mux)
-		if err != nil {
-			errCh <- err
-		}
-	}()
-
-	log.Println("start on http://localhost:6666")
-
-	for {
-		select {
-		case err := <-errCh:
-			if err == kandinsky.ErrTaskNotCompleted {
-				log.Println(err)
-				continue
-			}
-			log.Fatal(err)
-		case  <-image:
-			i := <-image
-			err := i.SaveJPGTo("name", "./")
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			log.Println("Image created")
-			break
-		}
+	resp, err := http.Get(frontendProxy.URL)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", b)
 
 }
